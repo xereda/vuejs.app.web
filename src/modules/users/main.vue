005e40397b5bb2c34b92b3e33f41624338572fb5
xereda <jacksonrs@gmail.com>
2016-10-07 12:06:59 -0300

    migrando para vuejs 2.0

--- src/modules/users/main.vue
+++ src/modules/users/main.vue
@@ -1,5 +1,4 @@
 <template>
-  <div>
     <section class="section">
       <div class="container">
         <h1 class="title">Cadastro de usuários</h1>
@@ -9,20 +8,14 @@
     <table class="table">
       <thead>
         <tr>
-          <th>Nome</th>
-          <th class="is-hidden-mobile">E-mail</th>
-          <th class="is-hidden-mobile">Admin?</th>
-          <th class="is-hidden-mobile">Ativo?</th>
+          <th v-for="col in collection" v-if="col.table && col.table.header && col.table.header.visible === true" :class="col.table.header.class">{{ col.label }}</th>
           <th></th>
           <th></th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="doc in docs">
-          <td>{{ doc.name }}</td>
-          <td class="is-hidden-mobile">{{ doc.email }}</td>
-          <td class="is-hidden-mobile">{{ (doc.admin) ? 'Sim' : 'Não' }}</td>
-          <td class="is-hidden-mobile">{{ (doc.active) ? 'Sim' : 'Não' }}</td>
+          <td v-for="col in collection" v-if="col.table && col.table.header && col.table.header.visible === true" :class="col.table.header.class">{{ doc[col.name] }}</td>
           <td class="is-icon">
             <a href="#">
               <i class="fa fa-folder-open"></i>
@@ -37,33 +30,43 @@
       </tbody>
     </table>
   </div>
+
+  {{ collection }}
+
 </section>
-  </div>
 </template>
 
 <script>
+// import _ from 'lodash'
 import { mapState, mapActions } from 'vuex'
 
 export default {
-  mounted () {
-    const payload = {
-      _id: '57a2bb75be785a529de5fb33',
-      name: 'Jackson Ricardo Schroeder',
-      email: 'jacksonrs@gmail.com',
-      lastSessionDate: new Date()
+  data () {
+    return {
+      docs: []
     }
-    setTimeout(() => {
-      this.updateUserSession(payload)
-    }, 2000)
+  },
+  mounted () {
+    this.getAll()
   },
   methods: {
-    ...mapActions(['updateUserSession'])
+    ...mapActions(['updateUserSession']),
+    getAll () {
+      const _params = ''
+      // GET /someUrl
+      this.$http.get('http://localhost:5000/users/?_fields=_id,name,email,active,admin' + _params).then((response) => {
+        this.docs = response.body
+      }, (response) => {
+        // error callback
+        console.log('erro no http: ', response)
+      })
+    }
   },
   computed: {
     ...mapState({
-      user: state => {
-        const { name, email, _id } = state.user
-        return `O usuário logado é ${name} e seu email é ${email} e seu _id é ${_id}.`
+      collection: state => {
+        const { collection } = state.users
+        return collection
       }
     })
   }
--- src/modules/users/store/state.js
+++ src/modules/users/store/state.js
@@ -1,3 +1,58 @@
 export default {
-  dadosDentroDoModulo: 'dados dentro de uma store de um modulo.'
+  collection: {
+    _id: { type: 'objectId' },
+    name: {
+      type: 'text',
+      label: 'Nome',
+      required: true,
+      table: {
+        header: {
+          visible: true,
+          class: ''
+        }
+      }
+    },
+    email: {
+      type: 'email',
+      label: 'E-mail',
+      required: true,
+      table: {
+        header: {
+          visible: true,
+          class: 'is-hidden-mobile'
+        }
+      }
+
+    },
+    admin: {
+      type: 'boolean',
+      label: 'Admin',
+      required: true,
+      table: {
+        header: {
+          visible: true,
+          class: 'is-hidden-mobile'
+        }
+      }
+
+    },
+    active: {
+      type: 'boolean',
+      label: 'Ativo',
+      required: true,
+      table: {
+        header: {
+          visible: true,
+          class: 'is-hidden-mobile'
+        }
+      }
+
+    },
+    createdById: {
+      type: 'objectId'
+    },
+    updatedById: {
+      type: 'objectId'
+    }
+  }
 }
