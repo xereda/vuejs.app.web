c603a46cdd57370f49baeeaa81741f4958889039
xereda <jacksonrs@gmail.com>
2017-02-08 18:49:47 -0200

    adicionado traducao para da mensagem para campo duplicado - name

--- src/modules/healthInsurances/components/Modal.vue
+++ src/modules/healthInsurances/components/Modal.vue
@@ -379,7 +379,11 @@ export default {
             _obj.message += res.data.err.errors[element].message + '<br />'
           })
         } else {
-          _obj.message += res.data.err.errmsg
+          if (res.data.err.errmsg.indexOf('E11000 duplicate key error index: docmob.healthinsurances.$name_1') > -1) {
+            _obj.message += 'Convênio informado já existe no cadastro!'
+          } else {
+            _obj.message += res.data.err.errmsg
+          }
         }
       }
       this.showAlerts(_obj)
