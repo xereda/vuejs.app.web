3cdc76c9c81df5bef7e267b0488af1ec7d10b276
xereda <jacksonrs@gmail.com>
2017-03-08 23:35:53 -0300

    novo componente para campo de telefone e um novo modulo para agrupar em um unico arquivo todos os componentes de formulario

---
+++ src/utils/ui/form/Phone.vue
@@ -0,0 +1,90 @@
+<template lang="html">
+<div>
+  <label v-if="showLabel" class="label">{{ label }}</label>
+  <p class="control has-icon">
+    <input :placeholder="placeholder" v-model="phone" ref="phone" :value="value" @input="applyMask($event.target.value)" @change="$emit('input', $event.target.value)" :class="{ 'input': true, 'is-danger': hasError }"></input>
+    <span class="icon is-small">
+      <i :class="faIcon"></i>
+    </span>
+    <span v-if="errorMessage !== ''" class="help is-danger">{{ errorMessage }}</span>
+  </p>
+</div>
+</template>
+
+<script>
+  import _ from 'lodash'
+  import VMasker from 'vanilla-masker'
+  import AwesomeMask from 'awesome-mask'
+
+  export default {
+    name: 'dmFormPhone',
+    data () {
+      return {
+        phone: ''
+      }
+    },
+    props: {
+      placeholder: {
+        type: String,
+        default: ''
+      },
+      value: {
+        type: String,
+        required: true
+      },
+      faIcon: {
+        type: String,
+        default: 'fa fa-font'
+      },
+      label: {
+        type: String,
+        default: ''
+      },
+      vuelidate: {
+        type: Object,
+        default: () => Object.assign({})
+      }
+    },
+    methods: {
+      applyMask (val) {
+        val.length > 14 ? this.phone = VMasker.toPattern(val, '(99) 99999-9999') : this.phone = VMasker.toPattern(val, '(99) 9999-9999')
+      }
+    },
+    computed: {
+      showLabel () {
+        return this.label !== undefined && this.label.length > 0
+      },
+      errorMessage () {
+        if (_.isEmpty(this.vuelidate)) {
+          return ''
+        }
+        if (this.vuelidate.$dirty !== undefined && this.vuelidate.$dirty === false) {
+          return ''
+        }
+        if (this.vuelidate.required !== undefined && this.vuelidate.required === false) {
+          if (this.label === '') {
+            return 'Campo é requerido!'
+          } else {
+            return this.label.replace(' *', '') + ' é requerido!'
+          }
+        }
+        if (this.vuelidate.minLength !== undefined && this.vuelidate.minLength === false) {
+          return 'Qt mínima de caracteres não atendida!'
+        }
+        if (this.vuelidate.maxLength !== undefined && this.vuelidate.maxLength === false) {
+          return 'Ultrapassou qt máxima de caracteres!'
+        }
+        return ''
+      },
+      hasError () {
+        return this.errorMessage !== ''
+      }
+    },
+    directives: {
+      'mask': AwesomeMask
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/utils/ui/form/main.js
@@ -0,0 +1,31 @@
+import DmFormInput from './Input.vue'
+import DmFormBoolean from './Boolean.vue'
+import DmButtons from './Buttons.vue'
+import DmFormCNPJ from './CNPJ.vue'
+import DmFormCPF from './CPF.vue'
+import DmFormDate from './Date.vue'
+import DmFormEmail from './Email.vue'
+import DmFormMutiSelect from './MultiSelect.vue'
+import DmFormName from './Name.vue'
+import DmFormRadio from './Radio.vue'
+import DmFormSelect from './Select.vue'
+import SubDocumentsList from './SubDocumentsList.vue'
+import DmFormTextarea from './Textarea.vue'
+import DmFormPhone from './Phone.vue'
+
+export {
+  DmFormInput,
+  DmFormBoolean,
+  DmButtons,
+  DmFormCNPJ,
+  DmFormCPF,
+  DmFormDate,
+  DmFormEmail,
+  DmFormMutiSelect,
+  DmFormName,
+  DmFormRadio,
+  DmFormSelect,
+  SubDocumentsList,
+  DmFormTextarea,
+  DmFormPhone
+}
